8cb5e8b01d714e3ae6bb9d95e00c049d7f85e8d2
alsoblank <luke.causer@outlook.com>
2024-09-08 16:02:54 +0100

    Updated builder to store the QASM script within

--- src/qasmbuilder/builder.py
+++ src/qasmbuilder/builder.py
@@ -11,6 +11,12 @@ class Builder:
         self.num_cbits = num_qubits # Maybe add choice later
         self.version = version
 
+        # Initiliase
+        self.qasm = f"OPENQASM {self.version}; \n"
+        self.qasm += f"include \"stdgates.inc\";\n\n"
+        self.qasm += f"qubit[{self.num_qubits}] q; \n"
+        self.qasm += f"bit[{self.num_cbits}] c; \n\n"
+
 
     # Checking that the qubit lives within the register 
     def _check_qubit(self, qubit: int):
@@ -18,78 +24,65 @@ class Builder:
             return True
         raise Exception("The specified qubits are not in the register.")
 
-    def initialise(self):
-        """
-        Initialise the QASM script.
-        """
-
-        qasm = f"OPENQASM {self.version}; \n"
-        qasm += f"include \"stdgates.inc\";\n\n"
-        qasm += f"qubit[{self.num_qubits}] q; \n"
-        qasm += f"bit[{self.num_cbits}] c; \n\n"
-        return qasm 
-
     ### Single body gates
     # Redundancy: each gate addition is basically the same, so lets streamline it.
     def _one_qubit_gate(self, qasm:str, qubit: int, gate: str):
         self._check_qubit(qubit)
-        qasm += f"{gate} q[{qubit}]; \n"
-        return qasm
+        self.qasm += f"{gate} q[{qubit}]; \n"
 
     def x(self, qasm: str, qubit: int):
         """
         Add an X gate at a qubit.
         """
-        return self._one_qubit_gate(qasm, qubit, "x")
+        self._one_qubit_gate(qasm, qubit, "x")
     
     def y(self, qasm: str, qubit: int):
         """
         Add a Y gate at a qubit.
         """
-        return self._one_qubit_gate(qasm, qubit, "y")
+        self._one_qubit_gate(qasm, qubit, "y")
     
     def z(self, qasm: str, qubit: int):
         """
         Add a Z gate at a qubit.
         """
-        return self._one_qubit_gate(qasm, qubit, "z")
+        self._one_qubit_gate(qasm, qubit, "z")
     
     def hadamard(self, qasm: str, qubit: int):
         """
         Add a Hadamard gate at a qubit.
         """
-        return self._one_qubit_gate(qasm, qubit, "h")
+        self._one_qubit_gate(qasm, qubit, "h")
 
     def rx(self, qasm: str, qubit: int, theta: float = np.pi):
         """
         A an X-rotation gate at a qubit. The angle of the rotation is theta.
         """
-        return self._one_qubit_gate(qasm, qubit, f"rx({theta})")
+        self._one_qubit_gate(qasm, qubit, f"rx({theta})")
     
     def ry(self, qasm: str, qubit: int, theta: float = np.pi):
         """
         A a Y-rotation gate at a qubit. The angle of the rotation is theta.
         """
-        return self._one_qubit_gate(qasm, qubit, f"ry({theta})")
+        self._one_qubit_gate(qasm, qubit, f"ry({theta})")
     
     def rz(self, qasm: str, qubit: int, theta: float = np.pi):
         """
         A a Z-rotation gate at a qubit. The angle of the rotation is theta.
         """
-        return self._one_qubit_gate(qasm, qubit, f"rz({theta})")
+        self._one_qubit_gate(qasm, qubit, f"rz({theta})")
     
 
     ### Two qubit gates
     # Redundancy: each gate addition is basically the same, so lets streamline it.
     def _two_qubit_gate(self, qasm:str, q1: int, q2: int, gate: str):
         self._check_qubit(q1) and self._check_qubit(q2)
-        qasm += f"{gate} q[{q1}] q[{q2}]; \n"
-        return qasm
+        self.qasm += f"{gate} q[{q1}] q[{q2}]; \n"
     
     def cnot(self, qasm: str, control: int, target: int):
         """
         Add a CNOT gate at a control qubit and target qubit.
         """
-        return self._two_qubit_gate(qasm, control, target, "cx")
+        self._two_qubit_gate(qasm, control, target, "cx")
     
 
--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 from src.qasmbuilder import builder
 
-builder = builder.Builder(6)
-qasm = builder.initialise()
+qasm = builder.Builder(6)
 
-qasm = builder.x(qasm, 0)
-qasm = builder.y(qasm, 1)
-qasm = builder.z(qasm, 2)
-qasm = builder.rx(qasm, 3, 0.565)
-qasm = builder.ry(qasm, 4, 0.123)
-qasm = builder.rz(qasm, 5, 0.05)
-qasm = builder.cnot(qasm, 0, 1)
-qasm = builder.cnot(qasm, 2, 3)
+qasm.x(qasm, 0)
+qasm.y(qasm, 1)
+qasm.z(qasm, 2)
+qasm.rx(qasm, 3, 0.565)
+qasm.ry(qasm, 4, 0.123)
+qasm.rz(qasm, 5, 0.05)
+qasm.cnot(qasm, 0, 1)
+qasm.cnot(qasm, 2, 3)
 
 
-print(qasm)
+print(qasm.qasm)
